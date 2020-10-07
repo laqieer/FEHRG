67603319e9a1b343543a2860c3b819bef1982fd7
laqieer <laqieer@126.com>
2020-10-07 20:55:20 +0800

    Change transparent color to mix into background color. https://github.com/laqieer/FEHRG/issues/5.

--- tool/script/convert_face.sh
+++ tool/script/convert_face.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while read line
 do
-    convert ../../portrait/full/${line}_Face.png -background skyblue -alpha remove -alpha off -dither None -colors 64 ../../graphic/portrait/full/${line}_Face.png
-    convert ../../portrait/mini/${line}_Face_FC.png -colorspace RGB -dither None -resize 32x32 -background aquamarine -alpha remove -alpha off -dither None -colors 16 ../../graphic/portrait/mini/${line}_Face_FC.png
+    convert ../../portrait/full/${line}_Face.png -background 'rgb(148,181,206)' -alpha remove -alpha off -dither None -colors 64 ../../graphic/portrait/full/${line}_Face.png
+    convert ../../portrait/mini/${line}_Face_FC.png -colorspace RGB -dither None -resize 32x32 -background 'rgb(165,165,255)' -alpha remove -alpha off -dither None -colors 16 ../../graphic/portrait/mini/${line}_Face_FC.png
 done < face_name.txt
