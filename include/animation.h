f1cd08f1c113337807167f79b41d3fa7c3e4b112
laqieer <laqieer@126.com>
2020-12-31 20:56:12 +0800

    Feature: support more animation script instructions. https://github.com/laqieer/FEHRG/issues/9

--- include/animation.h
+++ include/animation.h
@@ -12,7 +12,7 @@ struct Animation
     /* 0E */ u16 nextRoundId;
     /* 10 */ u16 state3;
     /* 12 */ u8 currentRoundType;
-    /* 13 */ u8 unk13;
+    /* 13 */ u8 frameID;
 
     /* 14 */ u8 commandQueueSize;
     /* 15 */ u8 commandQueue[7];
@@ -22,7 +22,7 @@ struct Animation
     /* 20 */ const u32* pScrCurrent;
     /* 24 */ const u32* pScrStart;
     /* 28 */ const void* pImgSheet;
-    /* 2C */ const void* pUnk2C;
+    /* 2C */ const void* pImgSheetBuffer;
     /* 30 */ const void* pSpriteDataPool; // aka "OAM data"
 
     /* 34 */ struct Animation* pPrev;
@@ -114,3 +114,39 @@ struct BattleAnimation {
 };
 
 extern const struct BattleAnimation BattleAnimations[];
+
+#define ANINS_IS_NOT_FORCESPRITE(instruction) ((instruction) & 0x80000000)
+#define ANINS_IS_PTRINS(instruction) ((instruction) & 0x40000000)
+
+#define ANINS_FORCESPRITE_GET_ADDRESS(instruction) ((void*) ((instruction) &~ 0xF0000003))
+#define ANINS_FORCESPRITE_GET_DELAY(instruction) ((((instruction) >> 26) & 0x1C) + ((instruction) & 3))
+
+#define ANINS_PTRINS_GET_TYPE(instruction) (0x3 & ((instruction) >> 28))
+#define ANINS_PTRINS_GET_ADDRESS(instruction) ((void*) ((instruction) &~ 0xF0000000))
+
+#define ANINS_GET_TYPE(instruction) (0x3F & ((instruction) >> 24))
+
+#define ANINS_WAIT_GET_DELAY(instruction) ((instruction) & 0xFFFF)
+
+#define ANINS_MOVE_GET_XOFF(instruction) (((int) ((instruction) << 24)) >> 24)
+#define ANINS_MOVE_GET_YOFF(instruction) (((int) ((instruction) << 16)) >> 24)
+#define ANINS_MOVE_GET_DELAY(instruction) (((instruction) >> 16) & 0xFF)
+
+#define ANINS_COMMAND_GET_ID(instruction) (0xFF & (instruction))
+
+#define ANINS_FRAME_GET_DELAY(instruction) ((instruction) & 0xFFFF)
+#define ANINS_FRAME_GET_FRAME(instruction) ((instruction) >> 16) & 0xFF
+
+enum
+{
+    ANIM_INS_TYPE_STOP    = 0,
+    ANIM_INS_TYPE_END     = 1,
+    ANIM_INS_TYPE_LOOP    = 2,
+    ANIM_INS_TYPE_MOVE    = 3,
+    ANIM_INS_TYPE_WAIT    = 4,
+    ANIM_INS_TYPE_COMMAND = 5,
+    ANIM_INS_TYPE_FRAME   = 6,
+};
+
+typedef void (*AnimCallback_t) (struct Animation* anim);
+
--- source/battle_animation.c
+++ source/battle_animation.c
@@ -58,3 +58,130 @@ const COLOR * getExtraAnimationPalette(s16 animationId)
     return getExtraAnimationPaletteCore(animationId);
 }
 
+// Expand animation script instructions.
+
+bool interpretAnimationCore(struct Animation* anim)
+{
+    int needsResort = FALSE;
+
+    u32 instruction = *anim->pScrCurrent++;
+
+    if (ANINS_IS_NOT_FORCESPRITE(instruction))
+    {
+        if (ANINS_IS_PTRINS(instruction))
+        {
+            switch (ANINS_PTRINS_GET_TYPE(instruction))
+            {
+
+            case 0: // function call
+                ((AnimCallback_t) (ANINS_PTRINS_GET_ADDRESS(instruction)))(anim);
+                break;
+
+            case 1: // set new frame data
+                anim->pScrStart   = ANINS_PTRINS_GET_ADDRESS(instruction);
+                anim->pScrCurrent = ANINS_PTRINS_GET_ADDRESS(instruction);
+
+                anim->timer = 1;
+
+                break;
+
+            } // switch (ANINS_PTRINS_GET_TYPE(instruction))
+        }
+        else
+        {
+            switch (ANINS_GET_TYPE(instruction))
+            {
+
+            case ANIM_INS_TYPE_STOP:
+                anim->pScrCurrent--;
+                anim->timer = 1;
+
+                anim->state2 = (anim->state2 & 0xFFF) | 0x4000;
+
+                break;
+
+            case ANIM_INS_TYPE_END:
+                anim->state = 0;
+                anim->timer = 1;
+
+                needsResort = TRUE;
+
+                break;
+
+            case ANIM_INS_TYPE_LOOP:
+                anim->pScrCurrent = anim->pScrStart;
+                anim->timer = 1;
+
+                break;
+
+            case ANIM_INS_TYPE_WAIT:
+                anim->timer = ANINS_WAIT_GET_DELAY(instruction);
+                break;
+
+            case ANIM_INS_TYPE_MOVE:
+                anim->xPosition += ANINS_MOVE_GET_XOFF(instruction);
+                anim->yPosition += ANINS_MOVE_GET_YOFF(instruction);
+
+                anim->timer = ANINS_MOVE_GET_DELAY(instruction);
+
+                break;
+
+            case ANIM_INS_TYPE_COMMAND:
+                anim->state2 = (anim->state2 & 0xFFF) | 0x1000;
+
+                anim->commandQueue[anim->commandQueueSize] = ANINS_COMMAND_GET_ID(instruction);
+                anim->commandQueueSize++;
+
+                anim->timer = 1;
+
+                switch (ANINS_COMMAND_GET_ID(instruction))
+                {
+
+                case ANIM_CMD_WAIT_01:
+                case ANIM_CMD_WAIT_02:
+                case ANIM_CMD_WAIT_03:
+                case ANIM_CMD_WAIT_04:
+                case ANIM_CMD_WAIT_05:
+                case ANIM_CMD_WAIT_13:
+                case ANIM_CMD_WAIT_18:
+                case ANIM_CMD_WAIT_2D:
+                case ANIM_CMD_WAIT_39:
+                case ANIM_CMD_WAIT_52:
+                    anim->pScrCurrent--;
+                    break;
+
+                } // switch (ANINS_COMMAND_GET_ID(instruction))
+
+                break;
+
+            case ANIM_INS_TYPE_FRAME:
+                anim->timer = ANINS_FRAME_GET_DELAY(instruction);
+                anim->frameID = ANINS_FRAME_GET_FRAME(instruction);
+
+                anim->pImgSheet = (const void*) (*anim->pScrCurrent++);
+
+                // ???????
+                anim->pSpriteData = (const void*) (*anim->pScrCurrent++);
+                anim->pSpriteData += (unsigned) anim->pSpriteDataPool;
+
+                anim->state2 = (anim->state2 & 0xFFF) | 0x2000;
+
+                break;
+
+            } // switch (0x3F & (instruction >> 24))
+        }
+    }
+    else
+    {
+        anim->pSpriteData = ANINS_FORCESPRITE_GET_ADDRESS(instruction);
+        anim->timer       = ANINS_FORCESPRITE_GET_DELAY(instruction);
+    }
+
+    return needsResort;
+}
+
+bool interpretAnimation(struct Animation* anim)
+{
+    return interpretAnimationCore(anim);
+}
+
