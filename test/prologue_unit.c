5d6f9a79d9f51c2f48be17481f1aab6b0c708b10
laqieer <laqieer@126.com>
2021-07-23 01:12:48 +0800

    Fix: reconvert Alfons & Sharon's pair animation with fixed conversion script. https://github.com/laqieer/FEHRG/issues/10.

--- test/prologue_unit.c
+++ test/prologue_unit.c
@@ -10,8 +10,8 @@
 
 const struct UnitDefinition playerUnitsInPrologue[] = {
     {
-        Tiki, // charIndex 
-        Mamkute, // classIndex
+        Alfons_pair01, // charIndex 
+        Sage, // classIndex
         0, // leaderCharIndex
         TRUE, // autolevel
         FACTION_BLUE, // allegiance
@@ -22,7 +22,7 @@ const struct UnitDefinition playerUnitsInPrologue[] = {
         7, // yPositionStart
         // items
         {
-            Flametongue,
+            Fire,
             0,
             0,
             0
