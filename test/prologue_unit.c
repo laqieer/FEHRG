a4b4c3221e4afeaf9457637e75f1b2133776b198
laqieer <laqieer@126.com>
2021-07-24 00:03:33 +0800

    Fix: reconvert Alfons's sword animation with fixed conversion script. https://github.com/laqieer/FEHRG/issues/10.

--- test/prologue_unit.c
+++ test/prologue_unit.c
@@ -10,8 +10,8 @@
 
 const struct UnitDefinition playerUnitsInPrologue[] = {
     {
-        Alfons_spring02, // charIndex 
-        Paladin, // classIndex
+        Alfons, // charIndex 
+        1, // classIndex
         0, // leaderCharIndex
         TRUE, // autolevel
         FACTION_BLUE, // allegiance
@@ -22,7 +22,7 @@ const struct UnitDefinition playerUnitsInPrologue[] = {
         7, // yPositionStart
         // items
         {
-            IronAxe,
+            IronSword,
             0,
             0,
             0
