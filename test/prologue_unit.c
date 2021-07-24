ec8607293ae7116539aa1a476915c34e05c413bc
laqieer <laqieer@126.com>
2021-07-24 18:54:48 +0800

    Fix: reconvert Reinhardt's magic animation with fixed conversion script. https://github.com/laqieer/FEHRG/issues/10.

--- test/prologue_unit.c
+++ test/prologue_unit.c
@@ -10,8 +10,8 @@
 
 const struct UnitDefinition playerUnitsInPrologue[] = {
     {
-        Rackesis, // charIndex 
-        Cleric, // classIndex
+        Reinhardt, // charIndex 
+        Valkyrie, // classIndex
         0, // leaderCharIndex
         TRUE, // autolevel
         FACTION_BLUE, // allegiance
@@ -22,7 +22,7 @@ const struct UnitDefinition playerUnitsInPrologue[] = {
         7, // yPositionStart
         // items
         {
-            Sleep,
+            Fire,
             0,
             0,
             0
