72ec2b9ce454b542f8e40796deaf44f5fa0c2412
laqieer <laqieer@126.com>
2021-07-24 19:34:44 +0800

    Fix: reconvert Sharon's lance animation with fixed conversion script. https://github.com/laqieer/FEHRG/issues/10.

--- test/prologue_unit.c
+++ test/prologue_unit.c
@@ -10,8 +10,8 @@
 
 const struct UnitDefinition playerUnitsInPrologue[] = {
     {
-        Roy_popularity01, // charIndex 
-        RoyLord, // classIndex
+        Sharon, // charIndex 
+        1, // classIndex
         0, // leaderCharIndex
         TRUE, // autolevel
         FACTION_BLUE, // allegiance
@@ -22,7 +22,7 @@ const struct UnitDefinition playerUnitsInPrologue[] = {
         7, // yPositionStart
         // items
         {
-            IronSword,
+            IronLance,
             0,
             0,
             0
