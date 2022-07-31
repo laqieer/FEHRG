9afeb7874a42f1176dbb81bccf93f2edf6919e3c
laqieer <laqieer@126.com>
2022-07-31 16:14:07 +0800

    Expand song table. https://github.com/laqieer/FEHRG/issues/17.

---
+++ source/music.c
@@ -0,0 +1,17 @@
+#include <tonc.h>
+
+#include "music.h"
+
+const struct Song * const pSongs1 = Songs;
+const struct Song * const pSongs2 = Songs;
+const struct Song * const pSongs3 = Songs;
+const struct Song * const pSongs4 = Songs;
+const struct Song * const pSongs5 = Songs;
+const struct Song * const pSongs6 = Songs;
+const struct Song * const pSongs7 = Songs;
+const struct Song * const pSongs8 = Songs;
+const struct Song * const pSongs9 = Songs;
+const struct Song * const pSongs10 = Songs;
+const struct Song * const pSongs11 = Songs;
+const struct Song * const pSongs12 = Songs;
+const struct Song * const pSongs13 = Songs;
