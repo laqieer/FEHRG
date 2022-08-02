abfa193ed17cdb2cb688afb1f3d02b99fbe578bd
laqieer <laqieer@126.com>
2022-08-02 18:22:09 +0000

    Pending changes exported from your codespace

--- tool/sound_room/main.c
+++ tool/sound_room/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "music_names_en.h"
+#include "decoding_tbl_cn.h"
 
 #define ENTRY_NUM 100
 #define BUFFER_SIZE 100
@@ -16,9 +18,33 @@ struct SoundRoomEntry {
 };
 
 struct SoundRoomEntry soundRoomEntries[ENTRY_NUM];
-char buffer[BUFFER_SIZE];
+unsigned char buffer[BUFFER_SIZE];
 
-void handleNameText(char *filename, int offset)
+void decodeCN()
+{
+    int length = strlen(buffer);
+    for (int i = 0; i < length && buffer[i] > 2; i++)
+    {
+        if (buffer[i] > 0x80)
+        {
+            int code = (buffer[i] << 8) + buffer[i + 1] - 0x8180;
+            if (code >= 0)
+            {
+                buffer[i] = decodingTbl[code][0];
+                if (decodingTbl[code][1])
+                    buffer[++i] = decodingTbl[code][1];
+                else
+                {
+                    if (i < length - 1)
+                        memmove(&buffer[i + 1], &buffer[i + 2], length-- - i - 2);
+                }
+            }
+        }
+    }
+    buffer[length] = 0;
+}
+
+void handleNameText(const char *filename, int offset, int isCN)
 {
     FILE *fp = fopen(filename,"rb");
     fseek(fp, offset, SEEK_SET);
@@ -27,22 +53,27 @@ void handleNameText(char *filename, int offset)
     {
         fseek(fp, soundRoomEntries[i].pNameText - ROM_BASE, SEEK_SET);
         fread(buffer, BUFFER_SIZE, 1, fp);
+        if (isCN)
+            decodeCN();
         printf("[TID_Song%03dName] = \"%s\",\n\n", soundRoomEntries[i].musicId, buffer);
     }
     fclose(fp);
 }
 
 int main() {
-    printf("\n--- Name Keys ---\n\n");
-    for (int i = 0; i < ENTRY_NUM; i++)
-        printf("TID_Song%03dName,\n", soundRoomEntries[i].musicId);
-
     printf("\n--- Name Texts (Japanese) ---\n\n");
-    handleNameText("../../baserom.gba", OFFSET_JP);
+    handleNameText("../../baserom.gba", OFFSET_JP, 0);
 
     printf("\n--- Name Texts (English) ---\n\n");
     for (int i = 0; i < ENTRY_NUM; i++)
         printf("[TID_Song%03dName] = \"%s\",\n\n", soundRoomEntries[i].musicId, musicNames[i]);
+
+    printf("\n--- Name Texts (Chinese) ---\n\n");
+    handleNameText("0033 - Fire Emblem - Rekka no Ken (Japan)[Wolf & Chinafe](v20050417) [CHS].gba", OFFSET_JP, 1);
+
+    printf("\n--- Name Keys ---\n\n");
+    for (int i = 0; i < ENTRY_NUM; i++)
+        printf("TID_Song%03dName,\n", soundRoomEntries[i].musicId);
 
     printf("\n--- sound room ---\n\n");
     for (int i = 0; i < ENTRY_NUM; i++)
