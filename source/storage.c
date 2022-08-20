7325dea75a6de5ba5271d73d3ada9b4388dcdf8a
laqieer <laqieer@126.com>
2022-08-21 02:15:54 +0800

    Unlock storage access in unit action menu.

--- source/storage.c
+++ source/storage.c
@@ -5,6 +5,11 @@ int isStorageAvailableInPrepScreen()
 {
     return TRUE;
 }
+// Always allow storage to be accessed in action menu
+int isStorageAvailableInActionMenu()
+{
+    return TRUE;
+}
 
 // Always allow items to be sent to storage when inventory is full
 int canSentToStorageIfFull()
