b1a673d6b9bf4236a6021aa31d9f9ac6dfe925c5
laqieer <laqieer@126.com>
2022-08-21 02:06:17 +0800

    Send item to storage if inventory is full.

--- source/storage.c
+++ source/storage.c
@@ -1,7 +1,13 @@
 #include <tonc.h>
 
-// Always allow storage to be accessed
-int isStorageAvailable()
+// Always allow storage to be accessed in prep screen
+int isStorageAvailableInPrepScreen()
+{
+    return TRUE;
+}
+
+// Always allow items to be sent to storage when inventory is full
+int canSentToStorageIfFull()
 {
     return TRUE;
 }
