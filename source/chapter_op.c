9c183dfc17337a8fac0ca30bdc62b9f6f6fd74db
laqieer <laqieer@126.com>
2022-08-21 15:45:09 +0800

    Chapter OP Logo.https://github.com/laqieer/FEHRG/projects/6.

---
+++ source/chapter_op.c
@@ -0,0 +1,7 @@
+#include <tonc.h>
+
+#include "all_gfx.h"
+
+cu32 * const pChapterOPLogoTiles = chapter_op_logoTiles;
+cu16 * const pChapterOPLogoPal = chapter_op_logoPal;
+cu16 * const pChapterOPLogoMap = chapter_op_logoMap;
